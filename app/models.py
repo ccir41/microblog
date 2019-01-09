0a8faa5ed8f60a9638ef94a13c608546709ee5b2
ccir41 <shishirsubedi41@gmail.com>
2019-01-09 21:40:47 +0545

    Created Models User and Post and Configured SQLite database

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128))
+
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<user {}>'.format(self.username)
+
+class Post(db.Model):
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}'.format(self.body)
